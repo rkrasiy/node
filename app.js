e76907f86ea4474618fbd5d5fc85768c19147e02
ruslan.krasiy <ruslan.krasiy@alten.es>
2024-05-20 18:54:32 +0200

    Start web server with Express & HBS

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
+require('dotenv').config();
 
-const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('./config/yargs');
-const colors = require('colors');
+const express = require('express');
+const hbs = require('hbs');
 
-console.clear();
+const app = express();
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
+
+//Serve static content;
+
+app.use( express.static('public'));
+
+app.get('/', function( req, res) {
+  res.render('home', {
+    nombre: 'Ruslan Krasiy',
+    titulo: 'Curso de Node JS'
+  });
+});
+
+app.get('/generic', function( req, res) {
+  res.render('generic', {
+    nombre: 'Ruslan Krasiy',
+    titulo: 'Curso de Node JS'
+  });
+});
+
+app.get('/elements', function( req, res) {
+  res.render('elements', {
+    nombre: 'Ruslan Krasiy',
+    titulo: 'Curso de Node JS'
+  });
+});
+
+app.get('*', function( req, res) {
+  res.sendFile(__dirname + '/public/404.html');
+});
 
 
-
-console.dir(argv)
-
-crearArchivo(argv.b, argv.l, argv.h)
-  .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
-  .catch( err => console.log(err));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
